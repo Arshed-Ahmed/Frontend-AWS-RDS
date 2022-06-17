4cb6a3da7234f65409119e8424125ed41eb1f161
Arshed <87371173+ahmed-arshed@users.noreply.github.com>
2022-06-17 10:05:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -360,7 +360,7 @@ app.post('/makecomplaints', (req, res)=> {
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, '../src/uploads'),
+    destination: (req, file, cb) => cb(null, 'src/uploads'),
     filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
 })
 const upload = multer({ storage: storage }); 
